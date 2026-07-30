f3e2312c80fc0d3f3c1c0fd3ef4b9369e9d8bd8b
Mehakbishnoi <bishnoimehak8@gmail.com>
2026-07-30 14:04:57 +0530

    Added Integer.valueOf() example

---
+++ wrapper_class/ValueOfExample.java
@@ -0,0 +1,15 @@
+// Program to demonstrate Integer.valueOf()
+
+public class ValueOfExample {
+
+    public static void main(String[] args) {
+
+        String number = "100";
+
+        // Converting String into Integer object
+        Integer obj = Integer.valueOf(number);
+
+        System.out.println("Integer Object: " + obj);
+
+    }
+}
